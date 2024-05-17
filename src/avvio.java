f62790021cf0f5ecdd91b566d1d235c919caf956
unknown <AntonioQuar>
2024-05-17 13:05:56 +0200

    creato menu

--- src/avvio.java
+++ src/avvio.java
@@ -1,37 +1,38 @@
-public class avvio {
-
+import java.util.Scanner;
+public class Avvio {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		gestoreStalla metodi = new gestoreStalla();
+		int scelta;
 		do {
+			metodi.menu();
 			System.out.println("");
-			System.out.println("Seleziona un'operazione: ");
+			System.out.println("Seleziona un metodo: ");
 			scelta = Integer.parseInt(input.nextLine());
 			switch (scelta) {
 			case 1:
-				aggiungiAnimale();
+				metodi.aggiungiAnimale();
 				System.out.println("Premi un tasto per continuare");
 				input.nextLine();
 				break;
 			case 2:
-				leggiAnimale(int id);
+				metodi.leggiAnimale();
 				System.out.println("Premi un tasto per continuare");
 				input.nextLine();
 				break;
-				case3:
-					leggiAnimali();
+			case 3:
+				metodi.leggiAnimali();
 				System.out.println("Premi un tasto per continuare");
 				input.nextLine();
 				break;
 			case 4:
-				rimuoviAnimale (int id);
+				metodi.rimuoviAnimali();
 				System.out.println("Premi un tasto per continuare");
 				input.nextLine();
 				break;
-			case 5:
-				exit;
-				break;
 			}
-			while (scelta !=5);
-			System.out.println("Statt buon.");
 		}
-
-	}
+		while (scelta !=5);
+		System.out.println("Statt buon.");
+	}	
+}
--- src/gestoreStalla.java
+++ src/gestoreStalla.java
@@ -1,4 +1,47 @@
-
+import java.util.Scanner;
 public class gestoreStalla {
+	String[] stalla = new String [5]; 
+	Scanner input = new Scanner(System.in);
+	int i = 0;
+	
+	void menu() {
+		System.out.println("");
+		System.out.println("   ***MENU***   ");
+		System.out.println("1) Aggiungi animale.");
+		System.out.println("2) visualizza animale.");
+		System.out.println("3) Visualizza tutti gli animali");
+		System.out.println("4) Rimuovi animale");
+		System.out.println("5) EXIT");
+	}
+	
+	void aggiungiAnimale() {
+		int id = Integer.parseInt(input.nextLine());
+		String tipoAnimale =  input.nextLine();
+		String provenienza = input.nextLine();
+		String alimentazione = input.nextLine();
+		double valore = Double.parseDouble(input.nextLine());
+		String animale = id + ";" + tipoAnimale + ";" + provenienza + ";" + alimentazione + ";" + valore;
+		String[] campi = animale.split(";");
+		System.out.println("Hai aggiunto: ");
+		System.out.println("id: " + id);
+		System.out.println("Tipo animale: " + tipoAnimale);
+		System.out.println("Provenienza: " + provenienza);
+		System.out.println("Alientazione: " + alimentazione);
+		System.out.println("Valore: " + valore);
+		stalla[i] = animale;
+		i++;
+		
+	}
+	void leggiAnimale() {
+	System.out.println("Inserire ID da ricercare:  ");
+	int idRicerca = Integer.parseInt(input.nextLine());
+	for (int i=0; i<stalla.length; i++) {
+	if (idRicerca ==  ) 
+	
+	}
+
+
+
+	}
 
 }
